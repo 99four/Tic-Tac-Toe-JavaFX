cd705db696ee482edf9d8a29431ff18db072c9f1
Damian Bachórz <damian.bachorz@briisk.co>
2016-10-31 16:47:38 +0100

    ini

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    public Button button;
+
+    public void handleButtonClick() {
+        System.out.println("cos sie dzieje");
+        button.setText("jeee");
+    }
+
+}
