41f222f2616a0d56638d808778685870a7aa4713
Damian Bachórz <damian.bachorz@briisk.co>
2016-10-31 22:34:39 +0100

    filling the board

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,42 @@
 package sample;
 
-import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 
 public class Controller {
+    //true for O, false for X
+    //schena
+    //a1 a2 a3
+    //b1 b2 b3
+    //c1 c2 c3
 
-    public Button button;
+    private char[] boardElements;
+    private boolean turn = false;
 
-    public void handleButtonClick() {
-        System.out.println("cos sie dzieje");
-        button.setText("jeee");
+    @FXML
+    protected void imageClicked(Event e) {
+        ImageView clickedImage = (ImageView)e.getSource();
+        if (turn) clickedImage.setImage(new Image("assets/o.bmp"));
+        else clickedImage.setImage(new Image("assets/x.bmp"));
+        clickedImage.setDisable(true);
+
+        if (this.checkIfThereIsAWinner()) System.out.println("We have a winner");
+        else turn = !turn;
     }
 
+    private boolean checkIfThereIsAWinner() {
+        return false;
+    }
+
+    private void fillTheArray() {
+        for (int i=0; i < 9; i++) {
+            this.boardElements[i] = '';
+        }
+        System.out.println(boardElements);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,10 +15,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         primaryStage.setTitle("Tic Tac Toe");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 480));
         primaryStage.show();
     }
-
-
 }
