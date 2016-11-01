31714c1abc83fa0c2cb72bde8b9c21bac381afec
Damian Bachórz <damian.bachorz@briisk.co>
2016-11-01 13:22:01 +0100

    proper logic

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,16 +9,13 @@ import java.util.List;
 
 
 public class Controller {
-    //true for O, false for X
-    //schena
-    //a1 a2 a3
-    //b1 b2 b3
-    //c1 c2 c3
+    // true for O, false for X
 
     @FXML
     private List<ImageView> boardElements;
 
     private boolean turn = false;
+    private short counter = 0;
 
     @FXML
     protected void imageClicked(Event e) {
@@ -26,10 +23,12 @@ public class Controller {
         if (turn) {
             clickedElement.setAccessibleText("O");
             clickedElement.setImage(new Image("assets/o.bmp"));
+            counter++;
         }
         else {
             clickedElement.setAccessibleText("X");
             clickedElement.setImage(new Image("assets/x.bmp"));
+            counter++;
         }
         clickedElement.setDisable(true);
 
@@ -38,23 +37,39 @@ public class Controller {
             for (ImageView boardElement : boardElements) {
                 boardElement.setDisable(true);
             }
-            System.out.println("We have a winner");
+            System.out.println("We have a winner, counter = " + counter);
         }
         else turn = !turn;
     }
 
     private boolean checkIfThereIsAWinner() {
-        int[] tab = {0, 1, 2};
-        return this.checkIfElementsAreEqual(tab);
+        // schema
+        // 0 1 2
+        // 3 4 5
+        // 6 7 8
+        int[][] arrayOfConditions = {
+                { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, // horizontal conditions
+                { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, // vertical conditions
+                { 0, 4, 8 }, { 2, 4, 6 } // diagonal conditions
+        };
+
+        for (int i = 0; i < arrayOfConditions.length; i++) {
+            if (this.checkIfElementsAreEqual(arrayOfConditions[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean checkIfElementsAreEqual(int[] indices) {
         String start = boardElements.get(indices[0]).getAccessibleText();
+        if (start.equals("N")) {
+            return false;
+        }
         for (int i = 1; i < indices.length; i++) {
-            if (!start.equals(boardElements.get(indices[i]).getAccessibleText()) || start.equals("N")) {
+            if (!start.equals(boardElements.get(indices[i]).getAccessibleText())) {
                 return false;
             }
-            start = boardElements.get(indices[i]).getAccessibleText();
         }
         return true;
     }
